d4bf207a7e5b6c256d82472c6582df2046c8b258
Lexi Dorrity <lexidorrity@Lexis-MBP.socal.rr.com>
2022-03-29 16:38:04 -0700

    finished manager and engineer cards, made HTML for some cards

---
+++ src/htmlGen.js
@@ -0,0 +1,34 @@
+const gemerateManagerCard = (manager) => {
+    return `
+    <div class="card">
+        <h3>${manager.getName()}</h3>
+            <p><i class="fa-solid fa-mug-hot"></i>${manager.gertRole()}</p>
+            <div class="card-body">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${manager.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a>
+                </li>
+                li class="list-group-item">Office Number: ${manager.getOfficeNumber()}</li>
+            </ul>
+        </div>
+    </div>
+`;
+};
+const gemerateManagerCard = (manager) => {
+    return `
+    <div class="card">
+        <h3>${manager.getName()}</h3>
+            <p><i class="fa-solid fa-mug-hot"></i>${manager.gertRole()}</p>
+            <div class="card-body">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${manager.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a>
+                </li>
+                li class="list-group-item">Office Number: ${manager.getOfficeNumber()}</li>
+            </ul>
+        </div>
+    </div>
+`;
+};
+
+
---
+++ src/index.js
@@ -0,0 +1,94 @@
+const inquirer = require("inquirer")
+const path = require("path")
+const fs = require("fs")
+
+const Engineer = require("./lib/Engineer")
+const Manager = require("./lib/Manager")
+const Intern = require("./lib/Intern")
+
+const teamMemberHtmlArr = [];
+
+
+function init() {
+    function managerCreate() {
+        inquirer.prompt([
+            {
+              type: 'input',
+              name: 'name',
+              message: 'What is the managers name?'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is the managers email?'
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is the Managers id?'
+            },
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: 'What is the office number?'
+            },
+        ])
+        .then(({name, id, email, officeNumber}) => {
+            const manager = new Manager(id, name, email, officeNumber)
+            teamMemberHtmlArr.push(generateManagerCard(manager))
+            mainMenu()
+        })
+    }
+    function mainMenu() {
+        inquirer.prompt([
+            {
+              type: 'list',
+              name: 'addedRole',
+              message: 'Would you like to add another employee?',
+              choices: ['Engineer', 'Intern', 'I am finished']
+            }, 
+        ])
+        .then(answers => {
+            switch (answers.addedRole) {
+                case "Engineer":
+                    return engineerCreate();
+                    case "Intern":
+                        return InternCreate();
+                    default:
+                        return generateHtml;
+            }
+        });
+    }
+    function engineerCreate() {
+        inquirer.prompt([
+            {
+              type: 'input',
+              name: 'name',
+              message: 'What is the engineers name?'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is the engineers email?'
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is the engineers id?'
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: 'What is your github?'
+            },
+        ])
+        .then(({name, id, email, github}) => {
+            const manager = new Engineer(id, name, email, github)
+            teamMemberHtmlArr.push(generateManagerCard(engineer))
+            mainMenu();
+        });
+    }
+    managerCreate();
+}
+
+init();

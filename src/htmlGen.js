8cf7f6a23bdddefb44fb0772df9902180b70de8b
Lexi Dorrity <lexidorrity@Lexis-MBP.socal.rr.com>
2022-04-04 09:08:45 -0700

    cleaned up code. fixed readme

--- src/htmlGen.js
+++ src/htmlGen.js
@@ -68,7 +68,6 @@ const baseHtml = (htmlArr) => `
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="../Assets/style.css">
     <title>Document</title>
 </head>
 <body class="page">
